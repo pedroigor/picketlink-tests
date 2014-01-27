9d7ec2c06c2d5bb126315d53514a99ec95896b35
Pedro Igor <pigor.craveiro@gmail.com>
2014-01-27 14:06:31 -0200

    Initial commit.

---
+++ federation/saml/src/test/java/org/picketlink/test/integration/federation/saml/QuickstartArchiveUtil.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.test.integration.federation.saml;
+
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
+/**
+ * @author Pedro Igor
+ */
+public class QuickstartArchiveUtil {
+
+    public static <T extends Archive> T resolveFromFederation(String artifactId) {
+        return (T) Maven.resolver().resolve("org.picketlink.quickstarts:" + artifactId + ":war:" + getFederationBindingClassifier() + ":" + getVersion())
+                   .withoutTransitivity()
+                   .asSingle(WebArchive.class);
+    }
+
+    private static String getFederationBindingClassifier() {
+        String arquillianLunch = System.getProperty("arquillian.launch");
+        String classifier = "wildfly-8";
+
+        if (arquillianLunch != null) {
+            if (arquillianLunch.startsWith("jboss-eap-")) {
+                classifier = "jboss-as7";
+            }
+        }
+        return classifier;
+    }
+
+    private static String getVersion() {
+        return System.getProperty("project.version", "2.6.0-SNAPSHOT");
+    }
+}
---
+++ federation/saml/src/test/java/org/picketlink/test/integration/federation/saml/SAMLWebBrowserSSOProfileTestCase.java
@@ -0,0 +1,89 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketlink.test.integration.federation.saml;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+import static org.picketlink.test.integration.federation.saml.QuickstartArchiveUtil.resolveFromFederation;
+
+/**
+ * @author Pedro Igor
+ */
+@RunWith (Arquillian.class)
+@RunAsClient
+public class SAMLWebBrowserSSOProfileTestCase {
+
+    @Deployment(name = "idp")
+    public static WebArchive deployIdp() {
+        return resolveFromFederation("idp");
+    }
+
+    @Deployment(name = "employee")
+    public static WebArchive deployEmployee() {
+        return resolveFromFederation("employee");
+    }
+
+    @Deployment(name = "sales")
+    public static WebArchive deploySales() {
+        return resolveFromFederation("sales-post");
+    }
+
+    @Test
+    @OperateOnDeployment("employee")
+    public void testSingleSignOn(@ArquillianResource URL url) throws Exception {
+        WebRequest request = new GetMethodWebRequest(url.toString());
+        WebConversation conversation = new WebConversation();
+        WebResponse response = conversation.getResponse(request);
+
+        WebForm webForm = response.getForms()[0];
+
+        webForm.setParameter("j_username", "tomcat");
+        webForm.setParameter("j_password", "tomcat");
+
+        webForm.getSubmitButtons()[0].click();
+
+        response = conversation.getCurrentPage();
+
+        assertTrue(response.getText().contains("EmployeeDashboard"));
+
+        request = new GetMethodWebRequest(url.toString().replace("/employee", "/sales-post"));
+        response = conversation.getResponse(request);
+
+        assertTrue(response.getText().contains("SalesTool"));
+    }
+
+}
