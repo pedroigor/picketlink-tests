e20b9e8c8fe1846e9e57366e33a9caaecf08be67
Pedro Igor <pigor.craveiro@gmail.com>
2014-02-15 00:52:16 -0200

    Tests for PLINK-363.

--- federation/saml/src/test/java/org/picketlink/test/integration/federation/saml/SAML11IDPInitiatedTestCase.java
+++ federation/saml/src/test/java/org/picketlink/test/integration/federation/saml/SAML11IDPInitiatedTestCase.java
@@ -28,29 +28,48 @@ import com.meterware.httpunit.WebForm;
 import com.meterware.httpunit.WebLink;
 import com.meterware.httpunit.WebRequest;
 import com.meterware.httpunit.WebResponse;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.picketlink.test.integration.federation.saml.QuickstartArchiveUtil.resolveFromFederation;
 
 /**
  * @author Pedro Igor
  */
-public abstract class AbstractServiceProviderTestCase extends AbstractFederationTestCase {
+@RunWith (Arquillian.class)
+@RunAsClient
+public class SAML11IDPInitiatedTestCase extends AbstractFederationTestCase {
+
+    @Deployment(name = "idp")
+    public static WebArchive deployIdentityProvider() {
+        return resolveFromFederation("idp");
+    }
+
+    @Deployment(name = "service-provider")
+    public static WebArchive deployServiceProvider() {
+        return resolveFromFederation("sales-saml11");
+    }
 
     @Test
-    @OperateOnDeployment("service-provider")
+    @OperateOnDeployment("idp")
     public void testAuthentication(@ArquillianResource URL url) throws Exception {
         WebConversation conversation = new WebConversation();
         HttpUnitOptions.setLoggingHttpHeaders(true);
         WebRequest request = new GetMethodWebRequest(formatUrl(url));
         WebResponse response = conversation.getResponse(request);
 
-        assertTrue(response.getURL().getPath().startsWith(getIdPContextPath()));
+        assertTrue(response.getURL().getPath().startsWith("/idp"));
         assertEquals(1, response.getForms().length);
 
         WebForm webForm = response.getForms()[0];
@@ -62,43 +81,12 @@ public abstract class AbstractServiceProviderTestCase extends AbstractFederation
 
         response = conversation.getCurrentPage();
 
-        doAssertAuthentication(response);
+        WebLink link = response.getLinkWithID("saml_11_sales_link");
+
+        assertNotNull(link);
+
+        response = link.click();
+
+        assertTrue(response.getText().contains("SalesTool"));
     }
-
-    @Test
-    @OperateOnDeployment("service-provider")
-    public void testLogout(@ArquillianResource URL url) throws Exception {
-        WebRequest request = new GetMethodWebRequest(formatUrl(url));
-        WebConversation conversation = new WebConversation();
-        WebResponse response = conversation.getResponse(request);
-
-        assertTrue(response.getURL().getPath().startsWith(getIdPContextPath()));
-        assertEquals(1, response.getForms().length);
-
-        WebForm webForm = response.getForms()[0];
-
-        webForm.setParameter("j_username", "tomcat");
-        webForm.setParameter("j_password", "tomcat");
-
-        webForm.getSubmitButtons()[0].click();
-
-        response = conversation.getCurrentPage();
-
-        doAssertAuthentication(response);
-
-        for (WebLink link: response.getLinks()) {
-            if (link.getURLString().contains("GLO=true")) {
-                link.click();
-            }
-        }
-
-        response = conversation.getResponse(conversation.getCurrentPage().getRefreshRequest());
-
-        assertTrue(response.getURL().getPath().startsWith(getIdPContextPath()));
-        assertEquals(1, response.getForms().length);
-    }
-
-    protected abstract String getIdPContextPath();
-    protected abstract void doAssertAuthentication(WebResponse response);
-
 }
