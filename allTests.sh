10d794e2a38deb2a1c187cda8a345f246f1360eb
Pedro Igor <pigor.craveiro@gmail.com>
2014-06-14 00:51:26 -0300

    Moving Base tests. Enabling more profiles.

---
+++ allTests.sh
@@ -0,0 +1,7 @@
+mvn clean verify -Pwildfly-common -Pwildfly-8.1.0.Final
+mvn clean verify -Pwildfly-common -Pwildfly-8.2.0.CR1-SNAPSHOT
+mvn clean verify -Pwildfly-common -Pwildfly-9.0.0.Alpha1-SNAPSHOT
+
+mvn clean verify -Pjboss-eap-common -Pjboss-eap-6.2
+mvn clean verify -Pjboss-eap-common -Pjboss-eap-6.3
+
---
+++ base/src/test/java/org/picketlink/test/idm/config/SingletonStartupEJBTestCase.java
@@ -0,0 +1,82 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.picketlink.test.idm.config;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.RelationshipManager;
+import org.picketlink.idm.model.basic.Group;
+import org.picketlink.idm.model.basic.Role;
+import org.picketlink.idm.model.basic.User;
+import org.picketlink.test.AbstractJPADeploymentTestCase;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertTrue;
+import static org.picketlink.idm.model.basic.BasicModel.addToGroup;
+import static org.picketlink.idm.model.basic.BasicModel.grantGroupRole;
+import static org.picketlink.idm.model.basic.BasicModel.grantRole;
+import static org.picketlink.idm.model.basic.BasicModel.hasGroupRole;
+import static org.picketlink.idm.model.basic.BasicModel.hasRole;
+import static org.picketlink.idm.model.basic.BasicModel.isMember;
+
+/**
+ * @author pedroigor
+ */
+@RunWith(Arquillian.class)
+public class SingletonStartupEJBTestCase extends AbstractJPADeploymentTestCase {
+
+    @Inject
+    private IdentityManager identityManager;
+
+    @Inject
+    private RelationshipManager relationshipManager;
+
+    @Deployment
+    public static WebArchive deploy() {
+        return deploy(SingletonStartupEJBTestCase.class, IDMInitializer.class);
+    }
+
+    @Test
+    public void testConfiguration() throws Exception {
+        User john = new User("manuela");
+
+        this.identityManager.add(john);
+
+        Role tester = new Role("Manager");
+
+        this.identityManager.add(tester);
+
+        Group qaGroup = new Group("Human Resources");
+
+        this.identityManager.add(qaGroup);
+
+        grantRole(relationshipManager, john, tester);
+        addToGroup(relationshipManager, john, qaGroup);
+        grantGroupRole(relationshipManager, john, tester, qaGroup);
+
+        assertTrue(hasRole(relationshipManager, john, tester));
+        assertTrue(isMember(relationshipManager, john, qaGroup));
+        assertTrue(hasGroupRole(relationshipManager, john, tester, qaGroup));
+    }
+
+}
